af1f59b9422629a385e31ec5270d72b4e2dec241
jehar <wakjeharkasan@gmail.com>
2020-08-06 22:19:56 +0800

    add new

--- clock.py
+++ clock.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-import server
+from server import sendback
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(server, 'interval', minutes=5)
+sched.add_job(sendback, 'interval', minutes=5)
 
 sched.start()
