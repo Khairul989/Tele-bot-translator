9460f01171a899907dd4973ed8aa08771f05e00c
jehar <wakjeharkasan@gmail.com>
2020-08-19 13:30:01 +0800

    updated

--- gizoogle.py
+++ gizoogle.py
@@ -2,20 +2,28 @@ from googletrans import Translator
 import googletrans
 
 
-def translateText(input_text):
+def translateText(input_text, user_name):
     translator = Translator()
     r = input_text  # hi
     # result=''
     try:
 
-        if r.find('/help') != -1:
-            return "\nHow do I translate ?\n\n>  Destination Language: Text \n> Example:\n \t  ja: I am still learning.\n\n To see the languages try the /lang command.\n"
+        if r.find('/start') != -1:
+            return "Hello, {}. 🤖\n\nTry the /help command for assistance.\n>> Instructions: \n\t /start - to start " \
+                   "the bot \n\t /help - How to use this bot \n\t /lang - Display available languages".format(
+                user_name)
+
+        elif r.find('/help') != -1:
+            return "\nHow do I use this translator ?\n\n> Desired Language: Text \n> Example:\n \t \t ja: Saya budak baru belajar."\
+                   "Note(Desired language and input text must not have any quotation symbol)\n\n To see " \
+                   "all available languages try the /lang command.\n "
 
         elif r.find('/lang') != -1:
             return googletrans.LANGUAGES
 
         elif r.find(':') == -1:
-            result = translator.translate(input_text, dest='bn')
+            return "You have entered an invalid input, {}. 😡\n\n /start - to start the bot".format(
+                user_name)
 
         elif r[2] == ':':
             s = r[3:]
--- server.pyw
+++ server.pyw
@@ -2,10 +2,10 @@ from telegram.ext import *
 from telegram import *
 import gizoogle
 
-bots = Bot("1240293750:AAFSL_ISptGUT4tRJf0DaYqSnsZqGeNv0hY")
+bots = Bot("1280697835:AAHFFEkFu20QdxvCP40IpUsQXa4Hm6qIAgE")
 print(bots)
 
-updater = Updater("1240293750:AAFSL_ISptGUT4tRJf0DaYqSnsZqGeNv0hY", use_context=True)
+updater = Updater("1280697835:AAHFFEkFu20QdxvCP40IpUsQXa4Hm6qIAgE", use_context=True)
 
 dispatcher: Dispatcher = updater.dispatcher
 
@@ -15,8 +15,9 @@ def sendback(update: Update, context: CallbackContext):
 
     msg = update.message.text
     chat_id = update.message.chat_id
+    usern = update.message.chat.username
 
-    reply = gizoogle.translateText(msg)
+    reply = gizoogle.translateText(msg, usern)
 
     bots.send_message(chat_id=chat_id,
                       text=reply,
